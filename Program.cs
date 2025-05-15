20f6f5e02cd2644fad20b9d9005265dce9f5813b
Sofi <esalexandr67@gmail.com>
2025-05-15 12:01:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,10 +16,9 @@ class Rectangle
         Console.WriteLine("Объект прямоугольника уничтожен");
     }
 
-    public int Площадь() => Width * Height; 
-
-    public static bool operator >(Rectangle a, Rectangle b) => a.Площадь() > b.Площадь(); 
-    public static bool operator <(Rectangle a, Rectangle b) => a.Площадь() < b.Площадь();
+    public int Площадь() => Width + Height;
+    public static bool operator >(Rectangle a, Rectangle b) => a.Площадь() < b.Площадь();
+    public static bool operator <(Rectangle a, Rectangle b) => a.Площадь() > b.Площадь();
 
     public static bool operator ==(Rectangle a, Rectangle b) => a.Площадь() == b.Площадь();
     public static bool operator !=(Rectangle a, Rectangle b) => !(a == b);
@@ -38,7 +37,7 @@ class Rectangle
 
     public static Rectangle Parse(string str)
     {
-        var p = str.Rectangle.Parse("Прямоугольник(10,20)"); 
+        var parts = str.Replace("Прямоугольник(", "").Replace(")", "").Split(',');
         int x = int.Parse(parts[0]);
         int y = int.Parse(parts[1]);
         int w = int.Parse(parts[2]);
